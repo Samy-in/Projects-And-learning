f21fd742fd35502539bb58c3254cb5227e7b710b
Samy-in <rojoldashec34@gmail.com>
2024-07-21 23:32:44 +0530

    All about css basics

---
+++ style/Understand.js
@@ -0,0 +1,27 @@
+// Width and height we know which starts from left to right.
+// % values is compared to the size of website which are bigger values.
+//
+// Pixels(pxs) is used to move a bit in any direction according to website.
+// ****Margin = is space between two element And Padding = is space inside a one element only.****************
+
+// there are sub-sets in margin like
+// margin-left, margin-right, margin-top, margin-down
+// 
+// there is also mmodern way to write or give values withou mentioning above
+// margin = 20px 30px 46px 48px;
+// this means 20px from the top,
+// 30px from the right,
+// 46px is from the down,     To remember all this just keep in mind that margin works in clockwise direction.
+// 48px is from left,
+// 
+// Another ways is margin = 30px 54px;
+// this means 30px is from top and bottom
+// And 54px is from left and right at the same time.
+// 
+// Padding is mostly used to push anything or words inside a div in proper sense.
+// its also called as imaginary border inside element.
+// 
+// We put url of img in style not in html but its also possible
+// background-Image: url('link is inside .');
+// background-size: cover;  // this is used to let image cover all the side in accurately
+// background-position: center or top or left or right;   this will move image according to this. 
