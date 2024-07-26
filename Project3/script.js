31af242f9f4c51370528dd69c94d391b50af71a5
Samy-in <rojoldashec34@gmail.com>
2024-07-27 01:20:57 +0530

    Project 3

--- Project3/script.js
+++ Project3/script.js
@@ -9,13 +9,13 @@ const arr = [
 // We have basically made a function that takes the arr in a parameter called clutter and keeps adding the images
 // and use a queryselector to select container and store this images as innerhtml.   
 function showTheCards (){
-    let clutter;
+     var clutter = "";
      arr.forEach(function(obj) {
         clutter += `<div class="box">
         <img class="cursor-pointer" src="${obj.img}" alt="image">
         <div class="caption">Lorem ipsum </div>
-    </div>`;
-     })
+         </div>`;
+     }),
 
      document.querySelector('.container').innerHTML = clutter;
 }
@@ -23,19 +23,29 @@ function showTheCards (){
 function handlesearchFunctionality(){
     const Input = document.querySelector('#searchinput');
     // this will make you only focus on the search bar and everything becomes dark.
-      Input.addEventListener('focus', function(){
-              document.querySelector(".overlay").style.display = 'block';
+      Input.addEventListener("focus", function(){
+              document.querySelector(".overlay").style.display = "block";
       }),
     // this will remove the focus on the search bar and bring light.  
-    Input.addEventListener('blur', function(){
-            document.querySelector('.overlay').style.display = 'none';
+    Input.addEventListener("blur", function(){
+            document.querySelector(".overlay").style.display = "none";
     })
 
     // 
-    Input.addEventListener('input', function(){
-           const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(Input.value))
+    Input.addEventListener("input", function(){
+           const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(Input.value));
+           var clutter = "";
+           filteredArray.forEach(function(obj){
+            clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+             </div>`
+           });
+
+           document.querySelector(".searchdata").style.display = 'block';
+           document.querySelector(".searchdata").innerHTML = clutter;
     })
     
 }
-handlesearchFunctionality()
-showTheCards()
+handlesearchFunctionality();
+showTheCards();
